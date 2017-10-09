eef2f3203ccf5fd782cf0bfb695ed761a4d1e589
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-09 23:42:31 +0600

    Old admin deleted
    preparation for active_admin

--- app/controllers/admin_controller.rb
+++
@@ -1,6 +0,0 @@
-class AdminController < ApplicationController
-  layout 'admin'
-  http_basic_authenticate_with name: "admin", password: "admin1"
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,7 @@ Rails.application.routes.draw do
   get 'all_places/index'
   get 'all_places/:id' => 'all_places#show', as: 'all_place' 
   root 'all_places#index'
-  get 'admin', to: 'admin#index', as: 'admin'
-  scope :admin do
-    resources :places, :dishes
-  end
-
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
