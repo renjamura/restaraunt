09e277074f20c314d630a9f7dbddc4e13c8929b9
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-10 01:40:09 +0600

    Finished with missing image
    Relocate missing image into assets/images

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,8 @@ class Place < ApplicationRecord
   has_many :dishes
 
   has_attached_file :image,
-                    styles: { medium: '300x300>', thumb: '100x100'},
-                    default_url: '/images/missing.png'
+                    styles: { medium: '300x300>', thumb: '100x100>'},
+                    default_url: 'missing.png'
   validates_attachment_content_type :image, 
                     content_type: ['image/jpeg','image/jpg', 'image/gif', 'image/png']
 
